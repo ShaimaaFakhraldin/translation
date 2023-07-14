7c5e9500b9a272fc60b03cb2332b792ac5848dd4
AMIT <shaimaa.a.fakhraldin@gmail.com>
2023-07-14 19:39:17 +0300

    init

---
+++ lib/lang_setting.dart
@@ -0,0 +1,51 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+class LangSetting extends StatefulWidget {
+  const LangSetting({super.key});
+
+  @override
+  State<LangSetting> createState() => _LangSettingState();
+}
+
+class _LangSettingState extends State<LangSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Change_Language'.tr()), centerTitle: true,),
+      body: Container(
+        alignment: Alignment.center,
+        margin:const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+
+             switchItem(locale: context.supportedLocales[0] , title: 'English'.tr()),
+             switchItem(locale: context.supportedLocales[1]  , title: 'Arabic'.tr())
+
+
+          ],
+        ),
+      ),
+    );
+  }
+   Widget  switchItem({ required Locale locale,   required String title}){
+    return
+      Container(
+      decoration:  BoxDecoration(
+          border: context.locale ==locale ?  Border.all(color: Colors.red) : null
+      ),
+      child:  ListTile(
+        title: Text(title),
+        onTap: () async{
+
+          await context.setLocale(locale);
+          setState(() {
+
+          });
+
+        },
+
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import 'lang_setting.dart';
+
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+
+  runApp(EasyLocalization(
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+      ],
+      path: 'assets/lang',
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  //Locale('en' ,'BS'),
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales ,
+      locale: context.locale ,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text( 'title'.tr()),
+        actions: [
+          IconButton(onPressed: (){
+             Navigator.push(context, MaterialPageRoute(builder: (_){ return LangSetting () ;}));
+          }, icon: Icon(Icons.language))
+        ],
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+             Text(
+              'button_title'.tr(),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
